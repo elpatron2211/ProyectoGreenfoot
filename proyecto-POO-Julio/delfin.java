a2b80d581145f1bdfbb90ca2fd4b6c7188a85c48
elpatron2211 <san21401@uvg.edu.gt>
2021-07-20 10:18:37 -0600

    Version inicial  proyecto
    Proyecto greenfoot

---
+++ proyecto-POO-Julio/delfin.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class delfin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class delfin extends Actor
+{
+    /**
+     * Act - do whatever the delfin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        mueveYvuelta();// Add your action code here.
+    }
+    public void mueveYvuelta()
+    {
+    move(4);
+    if (Greenfoot.isKeyDown("left"))
+    {
+        turn(-3);
+    }
+    if(Greenfoot.isKeyDown("right"))
+    {
+        turn(3);
+    }
+    }
+}
